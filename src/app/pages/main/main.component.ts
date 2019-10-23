9591cdc57e13c32bf5c953103a06787f2cf3867b
ElenaPetr <olpetrushova@gmail.com>
2019-10-23 16:02:46 +0300

    main page content

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  services = [
+    {
+      name: 'Coloring',
+      description: 'Ut enim ad minim veniam, quis trud exercitation...',
+      img: 's1.png'
+    },
+    {
+      name: 'Hairstyle',
+      description: 'Nemo enim ipsam voluptatem quia voluptas',
+      img: 's3.png'
+    },
+    {
+      name: 'Haircut',
+      description: 'Consectetur adipisicing elit, sed doe eiusmod',
+      img: 's4.png'
+    },
+    {
+      name: 'Coloring',
+      description: 'Ut enim ad minim veniam, quis trud exercitation...',
+      img: 's1.png'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
