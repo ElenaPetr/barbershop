1d91b195f3e80691ea6f58bc62248d3691dd3115
ElenaPetr <olpetrushova@gmail.com>
2019-10-23 14:28:40 +0300

    project structure, routing, header content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: 'main', component: MainComponent},
+  { path: 'about-us', loadChildren: () => import('./pages/about-us/about-us.module').then(mod => mod.AboutUsModule) },
+  { path: 'basket', loadChildren: () => import('./pages/basket/basket.module').then(mod => mod.BasketModule) },
+  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(mod => mod.ProductsModule) },
+  { path: 'services', loadChildren: () => import('./pages/services/services.module').then(mod => mod.ServicesModule) },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './pages/main/main.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { FragmentsModule } from './fragments/fragments.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FragmentsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
